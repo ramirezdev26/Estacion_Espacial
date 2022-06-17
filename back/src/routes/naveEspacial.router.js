634c2c4d253e1bcf9e0dcfb2600f32a2204b2b15
ramirezdev26 <santiago.ramirezc26@gmail.com>
2022-06-17 11:04:18 -0500

    Implementation of the CRUD in naveEspacial

--- back/src/routes/naveEspacial.router.js
+++ back/src/routes/naveEspacial.router.js
@@ -11,11 +11,70 @@ router.get('/', async function (req, res, next) {
 
       res.status(200).json({
           data: spacecraft,
-          alert: 'spacecrafts listed'
+          message: 'Spacecrafts listed'
       })
   } catch (err) {
       next(err);
   }
 });
+
+router.get('/:spacecraftId', async function (req, res, next) {
+  const { spacecraftId } = req.params;
+  try {
+    const spacecraft = await service.find({spacecraftId});
+
+    res.status(200).json({
+      data: spacecraft,
+      message: 'Spacecraft geted'
+    })
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post('/', async function (req, res, next) {
+  const request = req.body;
+  try {
+    const addSpacecraft = await service.createSpacecraft(request);
+
+    res.status(201).json({
+      data: addSpacecraft,
+      message: 'Spacecraft added'
+    });
+  } catch (err) {
+      next(err)
+    }
+  });
+
+  router.put('/:spacecraftId', async function (req, res, next) {
+    const { spacecraftId } = req.params;
+    const { body: message } = req;
+
+    try {
+        const updateSpacecraft = await service.updateSpacecraft({ spacecraftId, message });
+
+        res.status(200).json({
+            data: updateSpacecraft,
+            message : 'Spacecraft was updated'
+        });
+    } catch (err) {
+        next(err);
+    }
+  });
+
+  router.delete('/:spacecraftId', async function (req, res, next) {
+      const { spacecraftId } = req.params;
+      try {
+          const deleteSpacecraftId = await service.deleteSpacecraft({spacecraftId});
+
+          res.status(200).json({
+              data: deleteSpacecraftId,
+              message: 'Spacecraft deleted'
+          });
+      } catch(err) {
+          next(err);
+      }
+  });
+
 
 module.exports = router;
--- back/src/services/naveEspacial.service.js
+++ back/src/services/naveEspacial.service.js
@@ -10,9 +10,29 @@ class NaveEspacialService {
 
 
   async findAll() {
-    const messages = await this.mongoDB.getAll(this.collection);
-    return messages || [];
+    const spacecraft = await this.mongoDB.getAll(this.collection);
+    return spacecraft || [];
+  };
+
+  async find({ spacecraftId } = {}) {
+    const spacecraft = await this.mongoDB.get( this.collection, spacecraftId );
+    return spacecraft;
   }
+
+  async createSpacecraft(data) {
+    const addSpacecraft = await this.mongoDB.create(this.collection, data);
+    return addSpacecraft;
+  };
+
+  async updateSpacecraft({ spacecraftId, message } = {}) {
+    const updateSpacecraft = await this.mongoDB.update( this.collection, spacecraftId, message );
+    return updateSpacecraft
+  };
+
+  async deleteSpacecraft({ spacecraftId } = {}) {
+    const deletedSpacecraft = await this.mongoDB.delete( this.collection, spacecraftId );
+    return deletedSpacecraft;
+  };
 
 };
 
