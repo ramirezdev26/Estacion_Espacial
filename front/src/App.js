fa4a873e1bac35948a113e857aa89c966688e2c9
ramirezdev26 <santiago.ramirezc26@gmail.com>
2022-06-18 18:46:19 -0500

    Implementation of Delete, post and update in frontend usinf a form and a table with buttons

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,31 @@
 import React, { Fragment, useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import SpaceList from "./components/SpaceList";
+import Form from "./components/Form";
 
 function App() {
 
+  const [newSpacecraft, setNewSpacecraft] = useState({
+    name: ' ',
+    activity: ' ',
+    origin: ' ',
+    tripulation: 0,
+    combustible: ' ',
+    state: ' ',
+    weight: ' ',
+    burden: ' ',
+    thrust: ' ',
+    height: ' ',
+    power: ' ',
+    objectOfStudy: ' ',
+    inOrbit: ' ',
+    speed: ' ',
+    mission: ' '
+  })
+
   const [spacecrafts, setSpacecrafts] = useState([])
+
+  const [listUpdated, setListUpdated] = useState(false)
 
   useEffect(() => {
     const getSpacecrafts = () => {
@@ -13,19 +34,23 @@ function App() {
         .then(res => setSpacecrafts(res))
     }
     getSpacecrafts()
-  }, [])
+    setListUpdated(false)
+  }, [listUpdated])
 
   return (
     <Fragment>
       <Navbar brand="Estacion Espacial" />
-      <div className="container">
-        <div className="row">
-          <div className="col-7">
+      <div className="container ms-0">
+        <div className="row" style={{float: 'center'}}>
+          <div className="col">
             <h2 style={{textAlign: 'center'}}>Spacecraft List</h2>
-            <SpaceList spacecrafts={spacecrafts}/>
+            <SpaceList spacecrafts={spacecrafts} listUpdated={listUpdated} setListUpdated={setListUpdated} newSpacecraft={newSpacecraft} setNewSpacecraft={setNewSpacecraft}/>
           </div>
-          <div className="col-5">
+        <div/>
+        <div className="row"></div>
+          <div className="col">
             <h2 style={{textAlign: 'center'}}>Spacecraft Form</h2>
+            <Form newSpacecraft={newSpacecraft} setNewSpacecraft={setNewSpacecraft} />
           </div>
         </div>
       </div>
--- front/src/components/SpaceList.js
+++ front/src/components/SpaceList.js
@@ -1,6 +1,60 @@
 import React from 'react';
 
-const SpaceList = ({spacecrafts}) => {
+const SpaceList = ({spacecrafts, setListUpdated, newSpacecraft, setNewSpacecraft}) => {
+
+  const handleDelete = id => {
+    const requestInit = {
+      method: 'DELETE'
+    }
+    fetch('http://localhost:8080/api/v1/spacecraft/' + id, requestInit)
+        .then(res => res.json())
+        .then(res => console.log(res))
+
+    setListUpdated(true)
+  }
+
+  let { name, type, activity, origin, tripulation, combustible, state, weight } = newSpacecraft
+
+  const handleUpdate = id => {
+    tripulation = parseInt(tripulation, 10);
+    //validate the data
+    if(name === '' || type === '' || activity === '' || origin === '' || tripulation < 0 || combustible === '' || state === '' || weight ==='' ) {
+      alert('Todos los campos son oblogatorios')
+      return
+    }
+
+    //query
+    const requestInit = {
+      method: 'PUT',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(newSpacecraft)
+    }
+    fetch('http://localhost:8080/api/v1/spacecraft/' + id, requestInit)
+        .then(res => res.json())
+        .then(res => console.log(res))
+
+    // restarting newSpacecraftState
+    setNewSpacecraft({
+      name: ' ',
+      activity: ' ',
+      origin: ' ',
+      tripulation: 0,
+      combustible: ' ',
+      state: ' ',
+      weight: ' ',
+      burden: ' ',
+      thrust: ' ',
+      height: ' ',
+      power: ' ',
+      objectOfStudy: ' ',
+      inOrbit: ' ',
+      speed: ' ',
+      mission: ' '
+    })
+
+    setListUpdated(true)
+  }
+
   return (
     <table className="table">
       <thead>
@@ -14,20 +68,45 @@ const SpaceList = ({spacecrafts}) => {
           <th>Combustible</th>
           <th>Estado</th>
           <th>Peso</th>
+          <th>Carga</th>
+          <th>Empuje</th>
+          <th>Altura</th>
+          <th>Potencia</th>
+          <th>Objeto de estudio</th>
+          <th>En orbita</th>
+          <th>Velocidad</th>
+          <th>Mision</th>
         </tr>
       </thead>
       <tbody>
         {spacecrafts.map((spacecrafts) => (
           <tr key={spacecrafts._id}>
-            <th>{spacecrafts._id}</th>
-            <th>{spacecrafts.name}</th>
-            <th>{spacecrafts.type}</th>
-            <th>{spacecrafts.activity}</th>
-            <th>{spacecrafts.origin}</th>
-            <th>{spacecrafts.tripulation}</th>
-            <th>{spacecrafts.combustible}</th>
-            <th>{spacecrafts.state}</th>
-            <th>{spacecrafts.weight}</th>
+            <td>{spacecrafts._id}</td>
+            <td>{spacecrafts.name}</td>
+            <td>{spacecrafts.type}</td>
+            <td>{spacecrafts.activity}</td>
+            <td>{spacecrafts.origin}</td>
+            <td>{spacecrafts.tripulation}</td>
+            <td>{spacecrafts.combustible}</td>
+            <td>{spacecrafts.state}</td>
+            <td>{spacecrafts.weight}</td>
+            <td>{spacecrafts.burden}</td>
+            <td>{spacecrafts.thrust}</td>
+            <td>{spacecrafts.height}</td>
+            <td>{spacecrafts.power}</td>
+            <td>{spacecrafts.objectOfStudy}</td>
+            <td>{spacecrafts.inOrbit}</td>
+            <td>{spacecrafts.speed}</td>
+            <td>{spacecrafts.mission}</td>
+            <td>
+              <div className="mb-3">
+                <button onClick={() => handleDelete(spacecrafts._id)} className="btn btn-danger">Delete</button>
+              </div>
+              <div className="mb-3">
+                <button onClick={() => handleUpdate(spacecrafts._id)} className="btn btn-success">Update</button>
+              </div>
+            </td>
+
         </tr>
         ))}
       </tbody>
